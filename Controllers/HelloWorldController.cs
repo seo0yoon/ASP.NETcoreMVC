b332e541cb0e99be1f5c7fc487afb2098fc762c6
seo0yoon <genious921225@gmail.com>
2022-09-29 00:14:04 +0900

    test

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "이 페이지는 헬로월드";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
